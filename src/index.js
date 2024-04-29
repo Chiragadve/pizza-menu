3f7236a7330487286abba0e923d9fc2b655ac89d
Chirag <chiragadve4@gmail.com>
2024-04-29 23:19:42 +0530

    Logic commit

--- src/index.js
+++ src/index.js
@@ -75,25 +75,6 @@ function Header() {
   );
 }
 
-function Menu() {
-  return (
-    <main className="menu">
-      <h2>Our Menu</h2>
-      <Pizza
-        name="Pizza Spinaci"
-        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-        photoName="pizzas/spinaci.jpg"
-        price={10}
-      />
-      <Pizza
-        name="Pizza Funghi"
-        ingredients="Tomato, mushrooms"
-        price={12}
-        photoName="pizzas/funghi.jpg"
-      />
-    </main>
-  );
-}
 function Pizza(props) {
   console.log(props);
 

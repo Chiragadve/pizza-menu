3200dfbc6dbfb4b19f2f40b8d440db95f7ada66a
Chirag <chiragadve4@gmail.com>
2024-05-05 13:45:08 +0530

    UI changes

--- src/index.js
+++ src/index.js
@@ -166,10 +166,6 @@ function Order({ closeHour, openHour }) {
         We're open from {openHour}:00 until {closeHour}: 00. Come visit us or
         order online
       </p>
-      <p>
-        We're open from {openHour}:00 until {closeHour}: 00. Come visit us or
-        order online
-      </p>
       <button className="btn">Order</button>
     </div>
   );

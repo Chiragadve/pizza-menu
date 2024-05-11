53c0cb4e7734be8f02c07c023b36a563f66ca5fa
Chirag <chiragadve4@gmail.com>
2024-05-11 16:06:51 +0530

    ui commit

--- src/index.js
+++ src/index.js
@@ -48,6 +48,16 @@ const pizzaData = [
   },
 ];
 
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
 function Header() {
   // const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
   const style = {};
